5ec3f3d16e3c07cf9b8a4ba72e3ef5f062088a74
adrianmarino <adrianmarino@gmail.com>
2021-05-01 11:59:15 -0300

    feat: segment by time and features

---
+++ database/views.js
@@ -0,0 +1,80 @@
+//
+//
+//
+// ---------------------------------------------------------------------------
+// Charts
+// ---------------------------------------------------------------------------
+db.createView(
+    'track_weekly_top_200_2018',
+    'track_weekly_top_200',
+    [
+        {
+            $match: { week_start: { $gte: '2018-01-01'}, week_end: { $lte: '2018-12-31' } }
+        }
+    ]
+);
+
+
+db.createView(
+    'track_weekly_top_200_2019',
+    'track_weekly_top_200',
+    [
+        {
+            $match: { week_start: { $gte: '2019-01-01'}, week_end: { $lte: '2019-12-31' } }
+        }
+    ]
+);
+//
+//
+//
+// ---------------------------------------------------------------------------
+// Artists:
+// ---------------------------------------------------------------------------
+db.track_features.aggregate([
+    { $group: { _id: "$artist" } },
+    { $project: { _id: 0, name: "$_id" } },
+    {$out: "artists"}
+]);
+db.artists.createIndex({ "name": 1 }, { unique: true });
+//
+//
+//
+// ---------------------------------------------------------------------------
+// Tracks
+// ---------------------------------------------------------------------------
+db.track_features.aggregate([
+    { $project: { artist: 1, name: 1, album: 1 } },
+    {$out: "tracks"}
+]);
+db.tracks.createIndex({ "name": 1 }, { unique: true });
+//
+//
+//
+// ---------------------------------------------------------------------------
+// Track features
+// ---------------------------------------------------------------------------
+db.createView(
+    'track_real_features',
+    'track_features',
+    [
+        {
+            $project: {
+                danceability: 1,
+                energy: 1,
+                loudness: 1,
+                speechiness: 1,
+                acousticness: 1,
+                instrumentalness: 1,
+                liveness: 1,
+                valence: 1,
+                tempo: 1,
+                time_signature: 1,
+                duration_ms : 1,
+                explicit: 1,
+                key: 1,
+                mode: 1
+            }
+        }
+    ]
+);
+
--- scripts/preprocessing.R
+++ scripts/preprocessing.R
@@ -1,10 +1,14 @@
 library(pacman)
-p_load(this.path)
+p_load(stringi, tidyverse, this.path)
 source(paste(this.path::this.dir(), '/lib/import.R', sep=''))
 
 import('lib/data-access')
 
 
-track_features <- get_collection('track_features')
-result <- track_features$find('{}', limit = 10)
-names(result)
+track_features_col <- get_collection('track_features')
+track_features <- as.data.frame(track_features_col$find('{}'))
+
+
+track_features.numeric <- track_features %>% select(where(is.numeric))
+
+names(track_features.numeric)
