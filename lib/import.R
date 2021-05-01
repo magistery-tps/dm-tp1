9cab18180ebd03d1428d7ed10b7481d348de96ce
adrianmarino <adrianmarino@gmail.com>
2021-05-01 20:52:29 -0300

    feat: first notebook

--- lib/import.R
+++ lib/import.R
@@ -1,8 +1,8 @@
 library(pacman)
-p_load(this.path)
+p_load('this.path')
 
 import <- function(src_file_path) {
-  path <- paste(this.path::this.dir(), '/', src_file_path, '.R', sep='')
+  path <- paste(this.dir(), '/', src_file_path, '.R', sep='')
   print(paste('Import', path, 'source file.'))
   source(path)
 }
