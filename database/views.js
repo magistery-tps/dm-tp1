82e854798543afa99f837bc343116596588a4cba
adrianmarino <adrianmarino@gmail.com>
2021-05-01 21:13:54 -0300

    feat: add all track num feature  columns hostograms

--- database/views.js
+++ database/views.js
@@ -5,34 +5,13 @@
 // Charts
 // ---------------------------------------------------------------------------
 db.createView(
-    'track_weekly_top_200_2018',
-    'track_weekly_top_200',
-    [
-        {
-            $match: { week_start: { $gte: '2018-01-01'}, week_end: { $lte: '2018-12-31' } }
-        }
-    ]
-);
-
-
-db.createView(
-    'track_weekly_top_200_2019',
-    'track_weekly_top_200',
-    [
-        {
-            $match: { week_start: { $gte: '2019-01-01'}, week_end: { $lte: '2019-12-31' } }
-        }
-    ]
-);
-
-db.createView(
     'artist_chats_avg',
     'track_weekly_top_200',
     [
         {
             $group: {
                 _id: "$artist", 
-                avg_position: { $avg: "$position" },
+                avg_position:       { $avg: "$position" },
                 avg_reproductions:  { $avg: "$reproductions"},
             }
         },
@@ -60,7 +39,7 @@ db.track_weekly_top_200.aggregate([
           avg_streams: "$avg_streams"}  
         },
     {$out: "charts_avg"}
-])
+]);
 
 
 // Top 1 sin duplicados (157)
@@ -85,18 +64,46 @@ db.track_weekly_top_200.aggregate([
        }
     },
     { $out: "track_weekly_top_1" }
-])
+]);
+// Este indice es necesario para el join ya el lookup hace buqueda aleatoria.
 db.track_weekly_top_1.createIndex({ "artist": 1 });
+
+
+// Top 10 sin duplicados (1570)
+db.track_weekly_top_200.aggregate([
+    { $match: { "position": { $in: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]} } },
+    { 
+       $group: {
+           _id: { 
+               position:      "$position",
+               week_start:    "$week_start",
+               week_end:      "$week_end",
+               track:         "$track",
+               artist:        "$artist",
+               reproductions: "$reproductions"
+           },
+            position:       { $last : "$position" },
+            week_start:     { $last : "$week_start" },
+            week_end:       { $last : "$week_end" },
+            track:          { $last : "$track" },
+            artist:         { $last : "$artist" },
+            reproductions:  { $last : "$reproductions" }
+       }
+    },
+    { $out: "track_weekly_top_10" }
+]);
+// Este indice es necesario para el join ya el lookup hace buqueda aleatoria.
+db.track_weekly_top_10.createIndex({ "artist": 1 });
 //
 //
 //
 // ---------------------------------------------------------------------------
-// Artists:
+// Artists
 // ---------------------------------------------------------------------------
 db.track_features.aggregate([
     { $group: { _id: "$artist" } },
     { $project: { _id: 0, name: "$_id" } },
-    {$out: "artists"}
+    { $out: "artists" }
 ]);
 db.artists.createIndex({ "name": 1 }, { unique: true });
 //
@@ -107,7 +114,7 @@ db.artists.createIndex({ "name": 1 }, { unique: true });
 // ---------------------------------------------------------------------------
 db.track_features.aggregate([
     { $project: { artist: 1, name: 1, album: 1 } },
-    {$out: "tracks"}
+    { $out: "tracks" }
 ]);
 db.tracks.createIndex({ "name": 1 }, { unique: true });
 //
@@ -116,42 +123,11 @@ db.tracks.createIndex({ "name": 1 }, { unique: true });
 // ---------------------------------------------------------------------------
 // Track features
 // ---------------------------------------------------------------------------
-db.createView(
-    'track_real_features',
-    'track_features',
-    [
-        {
-            $project: {
-                danceability: 1,
-                energy: 1,
-                loudness: 1,
-                speechiness: 1,
-                acousticness: 1,
-                instrumentalness: 1,
-                liveness: 1,
-                valence: 1,
-                tempo: 1,
-                time_signature: 1,
-                duration_ms : 1,
-                explicit: 1,
-                key: 1,
-                mode: 1
-            }
-        } //, { $limit: 200 }
-    ]
-);
 
-db.createView(
-    'track_real_feature_200',
-    'track_features',
-    [
-        { $limit: 200 }
-    ]
-);
-db.track_real_feature_200.createIndex({ "artist": 1 }, { unique: true });
+// Indice (No unico) sobre la columns artist.
+db.track_features.createIndex({ "artist": 1 });
 
 // Join del top 1 con track features
-db.track_features.createIndex({ "artist": 1 });
 db.track_features.aggregate([
     {
       $lookup:
@@ -177,29 +153,79 @@ db.track_features.aggregate([
             tempo: 1,
             time_signature: 1,
             duration_ms: 1,
-            track:    {"$arrayElemAt": ["$charts.track", 0]},
-            artist2:    {"$arrayElemAt": ["$charts.artist", 0]},
-            week_start:    {"$arrayElemAt": ["$charts.week_start", 0]},
-            week_end:      {"$arrayElemAt": ["$charts.week_end", 0]},
-            position:      {"$arrayElemAt": ["$charts.position", 0]},
-            reproductions: {"$arrayElemAt": ["$charts.reproductions", 0]}
+            track:         { "$arrayElemAt": ["$charts.track", 0] },
+            artist2:       { "$arrayElemAt": ["$charts.artist", 0] },
+            week_start:    { "$arrayElemAt": ["$charts.week_start", 0] },
+            week_end:      { "$arrayElemAt": ["$charts.week_end", 0] },
+            position:      { "$arrayElemAt": ["$charts.position", 0] },
+            reproductions: { "$arrayElemAt": ["$charts.reproductions", 0] }
         }
     },
     {
         $match: { 
-            week_start:     { $exists:  true },
+            week_start:    { $exists:  true },
             week_end:      { $exists:  true },
             position:      { $exists:  true },
             reproductions: { $exists:  true },
-            $expr: {$eq: ["$artist", "$artist2"]},
-            $expr: {$eq: ["$name", "$track"]}
+            $expr:         { $eq: ["$artist", "$artist2"] },
+            $expr:         { $eq: ["$name", "$track"] }
         }
     },
     {
-        $project: {
-            name: 0,
-            artist2: 0
-        }
+        $project: { name: 0, artist2: 0 }
     },
     {$out: "track_features_top_1"}
-])
+]);
+
+
+
+// Join del top 10 con track features
+db.track_features.aggregate([
+    {
+      $lookup:
+        {
+          from: "track_weekly_top_10",
+          foreignField: "track",
+          localField: "name", 
+          as: "charts"
+        }
+   },
+   {
+        $project: {
+            artist: 1,
+            name: 1,
+            danceability: 1, 
+            energy: 1,
+            loudness: 1,
+            speechiness: 1,
+            acousticness: 1,
+            instrumentalness: 1,
+            liveness: 1,
+            valence: 1,
+            tempo: 1,
+            time_signature: 1,
+            duration_ms: 1,
+            track:         { "$arrayElemAt": ["$charts.track", 0] },
+            artist2:       { "$arrayElemAt": ["$charts.artist", 0] },
+            week_start:    { "$arrayElemAt": ["$charts.week_start", 0] },
+            week_end:      { "$arrayElemAt": ["$charts.week_end", 0] },
+            position:      { "$arrayElemAt": ["$charts.position", 0] },
+            reproductions: { "$arrayElemAt": ["$charts.reproductions", 0] }
+        }
+    },
+    {
+        $match: { 
+            week_start:    { $exists:  true },
+            week_end:      { $exists:  true },
+            position:      { $exists:  true },
+            reproductions: { $exists:  true },
+            $expr:         { $eq: ["$artist", "$artist2"] },
+            $expr:         { $eq: ["$name", "$track"] }
+        }
+    },
+    {
+        $project: { name: 0, artist2: 0 }
+    },
+    {$out: "track_features_top_10"}
+]);
+
--- lib/data-access.R
+++ lib/data-access.R
@@ -4,3 +4,9 @@ p_load(mongolite)
 get_collection <- function(name, db="spotify") { 
   mongo(collection=name, db = db)
 }
+
+get_collection_fields <- function(collection_name, field_name) {
+  collection <- get_collection(collection_name)
+  projection <- paste('{ "', field_name, '": 1 }', sep='')
+  collection$find(fields = projection)
+}
