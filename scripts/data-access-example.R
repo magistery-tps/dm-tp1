9ff99e11bf4813860525c447b6d3732c93eddca9
adrianmarino <adrianmarino@gmail.com>
2021-05-13 21:25:44 -0300

    feat: posicition vs features

--- scripts/data-access-example.R
+++ scripts/data-access-example.R
@@ -1,5 +1,5 @@
 library(pacman)
-p_load(this::path)
+p_load(this::path, tidyverse, WVPlots, GGally)
 setwd(this.path::this.dir())
 source('../lib/data-access.R')
 
@@ -60,7 +60,7 @@ track_features_top_10 %>%
   group_by(artist_track) %>%
   summarise(median_positon = median(position)) %>%
   arrange(median_positon) %>%
-  slice_head(n = 50) %>%
+  slice_head(n = 10) %>%
   mutate(name = fct_reorder(artist_track, desc(median_positon))) %>%
   ggplot( aes(x=name, y=median_positon)) +
   geom_bar(stat="identity", fill="#69b3a2", alpha=.6, width=.4) +
@@ -68,9 +68,16 @@ track_features_top_10 %>%
   xlab("") +
   theme_bw()
 
-track_features_top_100 <- get_track_features('track_features_top_100')
+
+
+
+track_features_top_200 <- get_track_features('track_features_top_200')
   
-position_features <- track_features_top_10 %>%
+num_cols <- c('danceability', 'energy', 'loudness', 'speechiness', 
+              'acousticness', 'instrumentalness', 'liveness',
+              'valence', 'tempo', 'duration_ms')
+
+position_features <- track_features_top_200 %>%
   group_by(position) %>%
   summarise(
     danceability = median(danceability),
@@ -84,15 +91,170 @@ position_features <- track_features_top_10 %>%
     tempo = median(tempo),
     duration_ms = median(duration_ms)
   ) %>%
-  mutate_at(
-    c('danceability', 'energy', 'loudness', 'speechiness', 
-      'acousticness', 'instrumentalness', 'livenes',
-      'valence', 'tempo', 'duration_ms'),
-    scale
-  ) %>%
-  arrange(desc(position))
+  mutate_at(num_cols, scale)
 
-ggplot(position_features, aes(x=position, y=danceability)) +
-  geom_area( fill="#69b3a2", alpha=0.5) +
-  geom_line(color="#69b3a2", size=1) +
-  ggtitle("Evolution of something")
+g1 <- qplot(
+  x=position, 
+  y=danceability, 
+  data = position_features, 
+  geom = c("point", "smooth"), 
+  formula='y ~ x', 
+  method = 'loess'
+)
+g2 <- qplot(
+  x=position, 
+  y=energy, 
+  data = position_features, 
+  geom = c("point", "smooth"), 
+  formula='y ~ x', 
+  method = 'loess'
+)
+g3 <- qplot(
+  x=position, 
+  y=loudness, 
+  data = position_features, 
+  geom = c("point", "smooth"), 
+  formula='y ~ x', 
+  method = 'loess'
+)
+g4 <- qplot(
+  x=position, 
+  y=speechiness, 
+  data = position_features, 
+  geom = c("point", "smooth"), 
+  formula='y ~ x', 
+  method = 'loess'
+)
+g5 <- qplot(
+  x=position, 
+  y=acousticness, 
+  data = position_features, 
+  geom = c("point", "smooth"), 
+  formula='y ~ x', 
+  method = 'loess'
+)
+g6 <- qplot(
+  x=position, 
+  y=instrumentalness, 
+  data = position_features, 
+  geom = c("point", "smooth"), 
+  formula='y ~ x', 
+  method = 'loess'
+)
+g7 <- qplot(
+  x=position, 
+  y=livenes, 
+  data = position_features, 
+  geom = c("point", "smooth"), 
+  formula='y ~ x', 
+  method = 'loess'
+)
+g8 <- qplot(
+  x=position, 
+  y=valence, 
+  data = position_features, 
+  geom = c("point", "smooth"), 
+  formula='y ~ x', 
+  method = 'loess'
+)
+g9 <- qplot(
+  x=position, 
+  y=tempo, 
+  data = position_features, 
+  geom = c("point", "smooth"), 
+  formula='y ~ x', 
+  method = 'loess'
+)
+g10 <- qplot(
+  x=position, 
+  y=duration_ms, 
+  data = position_features, 
+  geom = c("point", "smooth"), 
+  formula='y ~ x', 
+  method = 'loess'
+)
+
+p_load(egg)
+ggarrange(
+  g1, g2, g3, g4, g5,
+  g6, g7, g8, g9, g10,
+  ncol=2
+)
+
+names(track_features_top_10)
+
+
+
+  
+position_features <- track_features_top_10 %>%
+    mutate(position2 = as.character(position)) %>%
+    select(
+      position2, 
+      danceability,
+      energy, 
+      loudness,
+      speechiness, 
+      acousticness,
+      instrumentalness,
+      liveness,
+      valence,
+      tempo, 
+      duration_ms
+    ) %>%
+    rename(position = position2) %>%
+    mutate_at(num_cols, scale)
+
+names(position_features)
+
+PairPlot(
+  position_features, 
+  colnames(position_features),
+  " ",
+  group_var = "position", 
+  palette=NULL
+)
+
+
+
+PairPlot(
+  position_features, 
+  colnames(position_features),
+  " ",
+  group_var = "position", 
+  palette=NULL
+)
++
+  ggplot2::scale_color_manual(values=unique(as.factor(position_features$position)))
+
+
+
+track_features_top_200 <- get_track_features('track_features_top_50')
+
+
+r <- track_features_top_200 %>%
+  group_by(position) %>%
+  summarise(
+    danceability = median(danceability),
+    energy = median(energy),
+    loudness = median(loudness),
+    speechiness = median(speechiness),
+    acousticness = median(acousticness),
+    instrumentalness = median(instrumentalness),
+    livenes = median(liveness),
+    valence = median(valence),
+    tempo = median(tempo),
+    duration_ms = median(duration_ms)
+  )
+  
+ncol(r)
+
+PairPlot(
+  r,
+  colnames(r)[2:11],  " ", 
+  group_var = "position", 
+  palette=NULL
+)
++ ggplot2::scale_color_manual(values=unique(as.factor(r$position)))
+
+
+
