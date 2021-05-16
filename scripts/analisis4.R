580b2bf5c40da8b4a6d48802396d58108f328f80
adrianmarino <adrianmarino@gmail.com>
2021-05-16 17:24:09 -0300

    feat: question 4 finished

--- scripts/analisis4.R
+++ scripts/analisis4.R
@@ -2,6 +2,7 @@ library(pacman)
 p_load(this::path, tidyverse, WVPlots, GGally, egg)
 setwd(this.path::this.dir())
 source('../lib/data-access.R')
+source('../lib/plot.R')
 
 num_cols <- c(
   'danceability', 'energy', 'loudness',
@@ -117,8 +118,87 @@ result_6_10 <-
   filter('week_end.top4-5' <= 'week_start') %>%
   rename ('weeks.top6-10' = weeks) %>%
   select('artist_track', 'weeks.top1', 'weeks.top2-3', 'weeks.top4-5', 'weeks.top6-10')
+
 head(result_6_10)
 
-nrow(result_6_10)
+
+matrix <- data.matrix(result_6_10[2:5])
+
+heatmap.2(
+  matrix,
+  cellnote   = matrix,
+  trace      = "none",        
+  notecol    = "black", 
+  main       = "Cantidad de semanas por rango de posiciones", 
+  margins    = c(11, 11),  
+  col        = terrain.colors(3, rev = T),  
+  dendrogram = "none",
+  labRow     = result_6_10$artist_track,
+  symm       = T, 
+  Rowv       = F, # Ordena la diagonal (en vez de dendograma)  
+)
+
+hist(table(matrix[,1]))
+
+
+
+top_1_1 <- result_6_10 %>% 
+  select(artist_track, weeks.top1) %>%
+  rename(weeks = 'weeks.top1') %>%
+  mutate(top_range='1-1')
+head(top_1_1)
+
+top_2_3 <- result_6_10 %>% 
+  select(artist_track, 'weeks.top2-3') %>%
+  rename(weeks = 'weeks.top2-3') %>%
+  mutate(top_range='2-3')
+head(top_2_3)
+
+top_4_5 <- result_6_10 %>% 
+  select(artist_track, 'weeks.top4-5') %>%
+  rename(weeks = 'weeks.top4-5') %>%
+  mutate(top_range='4-5')
+head(top_4_5)
+
+top_6_10 <- result_6_10 %>% 
+  select(artist_track, 'weeks.top6-10') %>%
+  rename(weeks = 'weeks.top6-10') %>%
+  mutate(top_range='6-10')
+head(top_6_10)
+
+
+r <- top_1_1 %>%
+  union(top_2_3) %>%
+  union(top_4_5) %>%
+  union(top_6_10)
+
+ggplot(r, aes(y=reorder(artist_track, weeks), x=weeks, fill=top_range))+
+  scale_fill_manual(
+    "Rango de posiciones", 
+    values = c(
+      "1-1" = "gold",
+      "2-3" = "tomato", 
+      "4-5" = "royalblue1",
+      "6-10"="grey30"
+    )
+  )+
+  theme_light() +
+  geom_bar(stat="identity") +
+  ggtitle("Permanencia en semanas en cada rango de posiciones")
+
+
+artirst_track_weeks <- r %>% group_by(artist_track) %>% summarise(weeks = sum(weeks))
+
+gplot_hist(
+  artirst_track_weeks$weeks,
+  ylab = "Frecuencia",
+  name = "Permanencia (Cantidad de semanas)",
+  line_size=1.05,
+  truncated_mean_value=0.05,
+  binwidth=8,
+  linetype="solid"
+)
+
+
 
 
